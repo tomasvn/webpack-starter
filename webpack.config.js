82a616f7533e457ab1be1818b20c59a814da3e5a
Ngoc Tu Nguyen <nguyenngoct2112@gmail.com>
2020-01-13 01:27:12 +0100

    chore: Move browserlist into package, update prod, dev conf

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const autoprefixer = require('autoprefixer');
 const DashboardPlugin = require("webpack-dashboard/plugin");
 
 module.exports = {
   mode: 'developement',
+  devtool: 'source-map',
   entry: path.resolve(__dirname, 'src'),
   module: {
     rules: [
@@ -23,24 +23,6 @@ module.exports = {
         use: {
           loader: require.resolve('css-loader') // require.resolve() returns string which contains path to the module
         }
-      },
-      {
-        loader: require.resolve('postcss-loader'),
-        options: {
-          ident: 'postcss',
-          plugins: () => [
-            require('postcss-flexbugs-fixes'),
-            autoprefixer({
-              browsers: [
-                '>1%',
-                'last 4 versions',
-                'Firefox ESR',
-                'not ie < 9',
-              ],
-              flexbox: 'no-2009',
-            }),
-          ],
-        },
       },
       {
         loader: require.resolve('sass-loader'),
--- webpackProd.config.js
+++ webpackProd.config.js
@@ -1,9 +1,11 @@
 const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
   mode: 'production',
+  devtool: 'cheap-source-map',
   entry: path.resolve(__dirname, 'src'),
   output: {
     filename: '[hash].bundle.js',
@@ -20,7 +22,14 @@ module.exports = {
             presets: ['@babel/preset-env']
           }
         }
-      }
+      },
+      {
+        loader: require.resolve('postcss-loader'),
+        options: {
+          ident: 'postcss',
+          plugins: () => [ require('postcss-flexbugs-fixes'), autoprefixer({ flexbox: 'no-2009' }) ],
+        },
+      },
     ]
   },
   plugins: [
